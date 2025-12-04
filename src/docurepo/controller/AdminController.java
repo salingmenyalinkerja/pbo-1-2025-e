2b479d58950337e493c2eb3d7e8233e5af46e5d6
salingmenyalinkerja <salingmenyalinkerja@gmail.com>
2025-12-04 10:32:00 +0700

    021240046

--- src/docurepo/controller/AdminController.java
+++ src/docurepo/controller/AdminController.java
@@ -1,7 +1,24 @@
 package docurepo.controller;
 
 public class AdminController {
-    public static void main(String[] args){
-        System.out.println("=====DEBUGGING=====");
+
+    public void login(String username, String password) {
+        System.out.println("Login diproses...");
+    }
+
+    public void tambahUser() {
+        System.out.println("User berhasil ditambah");
+    }
+
+    public void editUser() {
+        System.out.println("User berhasil diedit");
+    }
+
+    public void hapusUser() {
+        System.out.println("User berhasil dihapus");
+    }
+
+    public void kelolaDokumen() {
+        System.out.println("Dokumen berhasil dikelola");
     }
 }
